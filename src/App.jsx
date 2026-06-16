d46172b14e864ff1e6779240a47bbcb88233e75c
Daniel <nisaki2003@gmail.com>
2026-06-16 10:13:18 +0200

    nav done & lil of home

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
 import { useState } from "react";
 import { Link, Outlet } from "react-router";
-import "./App.css";
+import styles from "./styles/App.module.css";
 
 function App() {
   const [view, setView] = useState("Home");
 
   return (
     <>
-      <div className="navbar">
-        <ul className="">
-          <li className="nav">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="nav">
-            <Link to="/Shop">Shop</Link>
-          </li>
-          <li className="nav">
-            <Link to="/cart">Shopping Cart</Link>
-          </li>
-        </ul>
-      </div>
+      <nav className={styles.navbar}>
+        <div className={styles.upperNavbar}>
+          <div className={styles.upperNavbarLogo}>
+          <h1>Hase</h1>
+          <h2>Sportz</h2>
+          </div>
+          <div className={styles.navIcons}>
+            <i>search</i>
+            <i>cart</i>
+          </div>
+        </div>
+        <div className={styles.lowerNavbar}>
+            <Link to="/" className={styles.navItem}>Home</Link>
+          
+          
+            <Link to="/Shop" className={styles.navItem}>Shop</Link>
+          
+          
+            <Link to="/cart" className={styles.navItem}>Shopping Cart</Link>
+          
+        </div>
+      </nav>
       <Outlet />
     </>
   );
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,22 @@
+import styles from "./styles/Home.module.css"
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <div className={styles.firstSection}>
+        <div className={styles.sectionText}>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium maxime voluptas quae quod. Ratione ducimus possimus error eum quo sit sequi neque recusandae, quia optio quas at tenetur corporis mollitia!</p>
+        </div>
+        <div className={styles.sectionImageWrapper}>
+          <img 
+        
+        src={`${import.meta.env.BASE_URL}home/shoes.jpeg`} 
+        alt="Awesome Backpack" 
+      />
+        </div>
+      </div>
+      </main>
+  );
 }
 
 export default Home;
