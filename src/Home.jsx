1526ba52ea1c896781bc7c5cb53682f07f2ddbc8
kinarkin <kinarkin@hs-pforzheim.de>
2026-06-20 17:25:06 +0200

    little work on home(image)

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,25 @@
-import styles from "./styles/Home.module.css"
+import styles from "./styles/Home.module.css";
 
 function Home() {
   return (
     <main>
       <div className={styles.firstSection}>
         <div className={styles.sectionText}>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium maxime voluptas quae quod. Ratione ducimus possimus error eum quo sit sequi neque recusandae, quia optio quas at tenetur corporis mollitia!</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+            maxime voluptas quae quod. Ratione ducimus possimus error eum quo
+            sit sequi neque recusandae, quia optio quas at tenetur corporis
+            mollitia!
+          </p>
         </div>
         <div className={styles.sectionImageWrapper}>
-          <img 
-        
-        src={`${import.meta.env.BASE_URL}home/shoes.jpeg`} 
-        alt="Awesome Backpack" 
-      />
+          <img
+            src={`${import.meta.env.BASE_URL}home/shoes.jpg`}
+            alt="Awesome Backpack"
+          />
         </div>
       </div>
-      </main>
+    </main>
   );
 }
 
